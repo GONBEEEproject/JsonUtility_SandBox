69a2aefc34003890e30be61af5d41317e63d5e58
GONBEEE_project_MPlexPC <gonbeee.project@gmail.com>
2018-11-29 17:30:55 +0900

    Save機構、BaseItem継承しても保存できるぞ？

--- Assets/My_Assets/Scripts/JsonTest.cs
+++ Assets/My_Assets/Scripts/JsonTest.cs
@@ -1,16 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.IO;
 
 public class JsonTest : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [ContextMenu("Save")]
+    void Save()
+    {
+        Item item = new Item();
+        item.ID = 1;
+        item.time = 0.1f;
+        item.hoge = "hogehoge";
+
+        item.BaseID = 100;
+        item.BaseTime = 100.0f;
+
+        string itemJson = JsonUtility.ToJson(item);
+        Debug.Log(itemJson);
+
+        string path = Application.streamingAssetsPath;
+
+        var writer = new StreamWriter(path + "\\hoge.json", false);
+        writer.WriteLine(itemJson);
+        writer.Flush();
+        writer.Close();
+
+    }
+}
+
+[Serializable]
+public class Item : ItemBase
+{
+    public int ID;
+    public float time;
+    public string hoge;
+}
+
+[Serializable]
+public class ItemBase
+{
+    public int BaseID;
+    public float BaseTime;
 }
